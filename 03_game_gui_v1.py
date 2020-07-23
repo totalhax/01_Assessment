f4c6c68460bbeb5b834bdae827f7f9c40b9af7bf
Reuben <48073558+totalhax@users.noreply.github.com>
2020-07-23 14:51:52 +1200

    broken check

--- 03_game_gui_v1.py
+++ 03_game_gui_v1.py
@@ -10,23 +10,54 @@ class Game:
         self.game_frame = Frame(padx=10, pady=10)
         self.game_frame.grid()
 
-        self.how_many_questions = Label(self.game_frame, text="1 + 2 = ", font="Arial 20")
-        self.how_many_questions.grid(row=0)
+        op = "/"
 
-        self.start_amount_entry = Entry(self.game_frame,
-                                        font="Arial  19 bold", width=13)
-        self.start_amount_entry.grid(row=1, column=0)
+        num1 = random.randint(1, 10)
+        num2 = random.randint(1, 10)
+
+        num3 = num1 * num2
+
+        if op == "/":
+            question = "{} {} {}".format(num3, op, num1)
+        else:
+            question = "{} {} {}".format(num1, op, num2)
+
+        eval(question)
+
+        print(eval(question))
+
+        self.question = Label(self.game_frame, text=question, font="Arial 20")
+        self.question.grid(row=0)
+
+        self.answer_entry = Entry(self.game_frame,
+                                  font="Arial  19 bold", width=13)
+        self.answer_entry.grid(row=1, column=0)
 
         self.button_frame = Frame(self.game_frame, width=200)
         self.button_frame.grid(row=2)
 
-        self.add_button = Button(self.button_frame, text="Submit",
-                                 width=12, height=2)
-        self.add_button.grid(row=0, column=0)
+        self.submit_button = Button(self.button_frame, text="Submit",
+                                    width=12, height=2, command=self.check_ans)
+        self.submit_button.grid(row=0, column=0)
 
-        self.add_button = Button(self.button_frame, text="Next",
-                                 width=12, height=2)
-        self.add_button.grid(row=0, column=1)
+        self.next_button = Button(self.button_frame, text="Next",
+                                  width=12, height=2)
+        self.next_button.grid(row=0, column=1)
+
+    def check_ans(self):
+
+        try:
+            answer = int(self.answer_entry.get())
+            if answer == eval(                                      ):
+                print("correct")
+
+            else:
+                print("wrong")
+
+        except ValueError:
+            print("number please")
+
+
 
 
 # main routine
