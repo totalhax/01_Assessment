b91b6cce30b609d266e2d7e8fe0217d00158c36a
Reuben <48073558+totalhax@users.noreply.github.com>
2020-07-31 13:29:35 +1200

    history_list

--- sandbox_2.py
+++ sandbox_2.py
@@ -1,17 +1,35 @@
 import random
 
-op = "*"
 
-for item in range(1, 5):
-    # make question
-    num1 = random.randint(1, 12)
-    num2 = random.randint(1, 12)
+complete_history = []
+single_question = []
 
-    question = "{} {} {}".format(num1, op, num2)
-    correct = eval(question)
+for item in range(1, 4):
+    num1 = random.randrange(1, 12)
+    num2 = random.randrange(1, 12)
 
-    ask = int(input("{} = ".format(question)))
-    if ask == correct:
-        print("great job")
-    else:
-        print("oops")
+    question = "{} + {}".format(num1, num2)
+    answer = eval(question)
+
+    user_ans = int(input(question))
+
+    single_question.append(question)
+    single_question.append(answer)
+    single_question.append(user_ans)
+
+    complete_history.append(single_question)
+    single_question = []
+
+print("complete history", complete_history)
+print(complete_history)
+print()
+
+row = 0
+for line in complete_history:
+    print("Question", complete_history[row][0])
+    print("Correct ans", complete_history[row][1])
+    print("User ans", complete_history[row][2])
+    print()
+
+
+    row += 1
